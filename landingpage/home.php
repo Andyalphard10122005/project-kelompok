eae1ef0c8e1a6316a2d2288988c9724f5936ae3d
andyalphard10122005 <Andoalpot@gmail.com>
2022-09-29 20:07:10 +0700

    Tambah Informasi rumah sakit

--- landingpage/home.php
+++ landingpage/home.php
@@ -6,12 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <!-- font ubuntu -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="kotak">
-        <p>Mencari Berdasarkan Nama Dokter</p>
-        <input type="text" class="input1" placeholder="Tulis nama Dokter"> <br>
-        <input type="date" class="input2" placeholder="Choose Date">
+    <!-- Consule -->
+    <!-- <div class="container1">
+        <div class="kotak">
+            <p>Mencari Berdasarkan Nama Dokter</p>
+            <input type="text" class="input1" placeholder="Tulis nama Dokter"> <br>
+            <input type="date" class="input2" placeholder="Choose Date">
+        </div>
+    </div> -->
+    
+
+    <!-- Informasi Rumah sakit -->
+    <div class="container2">
+        <div class="tempatfoto">
+        </div>
+        <div class="text">
+            <p>WeCare Hospital @KitaPeduli</p>
+            <p>WeCare Hospital adalah rumah sakit  <br> yang melayani jemis penyakit seperti <br> Mata......</p>
+        </div>
     </div>
+    
+    
 </body>
 </html>
