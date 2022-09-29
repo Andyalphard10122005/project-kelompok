08be080a0cc07f57ba3bd9363753b889dd2978dc
andyalphard10122005 <Andoalpot@gmail.com>
2022-09-29 18:10:39 +0700

    Tambah consul

---
+++ landingpage/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kotak">
+        <p>Mencari Berdasarkan Nama Dokter</p>
+        <input type="text" class="input1" placeholder="Tulis nama Dokter"> <br>
+        <input type="date" class="input2" placeholder="Choose Date">
+    </div>
+</body>
+</html>
